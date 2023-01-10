210448ae1f1b5d73b89eb05ce019591e94f22a37
Pavel Botos <Pavel.BOTOS@ext.ec.europa.eu>
2023-01-10 23:17:16 +0100

    Added example of illegal attempt to access implementation.

---
+++ multi-module-example-integration/multi-module-example-integration-01/src/main/java/org/botpave/integration/controllers/rest/ExampleRestFailingController.java
@@ -0,0 +1,15 @@
+package org.botpave.integration.controllers.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+//import org.botpave.business.impl.ExampleBusinessServiceImpl;
+
+@RestController
+public class ExampleRestFailingController {
+
+//    private final ExampleBusinessServiceImpl exampleBusinessService;
+//
+//    public ExampleRestFailingController(ExampleBusinessServiceImpl exampleBusinessService){
+//        this.exampleBusinessService = exampleBusinessService;
+//    }
+
+}
